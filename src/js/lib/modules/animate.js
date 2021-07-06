db16ecc6c0ba0952ee68ebf84e9e0cf791913b4a
Georgiy Filippov <g.filippov777@gmail.com>
2021-07-06 21:48:47 +0500

    add `fadeToggle` method

--- src/js/lib/modules/animate.js
+++ src/js/lib/modules/animate.js
@@ -1,6 +1,12 @@
 import Lib from '../core'
 
-Lib.prototype.fadeIn = function (duration = 800) {
+/**
+ * Create fadeIn method
+ * @param {number} duration Animation duration
+ * @param {string} display block | flex | grid
+ * @returns this
+ */
+Lib.prototype.fadeIn = function (duration = 800, display = 'block') {
   function animateFadeIn(elem, dur = 800, iterations = 1) {
     const keyframes = [
       { opacity: '0', offset: 0 },
@@ -13,12 +19,18 @@ Lib.prototype.fadeIn = function (duration = 800) {
 
   for (let i = 0; i < this.length; i++) {
     const element = this[i]
+    element.style.display = display
     animateFadeIn(element, duration)
   }
 
   return this
 }
 
+/**
+ * Create fadeOut method
+ * @param {number} duration Animation duration
+ * @returns this
+ */
 Lib.prototype.fadeOut = function (duration = 800) {
   function animateFadeOut(elem, dur = 800, iterations = 1) {
     const keyframes = [
@@ -37,6 +49,25 @@ Lib.prototype.fadeOut = function (duration = 800) {
     setTimeout(() => {
       element.style.display = 'none'
     }, duration)
+  }
+
+  return this
+}
+
+/**
+ * Create fadeToggle method
+ * @param {number} duration Animation duration
+ * @returns this
+ */
+Lib.prototype.fadeToggle = function (duration = 800) {
+  for (let i = 0; i < this.length; i++) {
+    const element = this[i]
+
+    if (window.getComputedStyle(element).display === 'none') {
+      Lib(element).fadeIn(duration)
+    } else {
+      Lib(element).fadeOut(duration)
+    }
   }
 
   return this
